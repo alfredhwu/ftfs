9a11341ed9f040bb36c2782381b325110e693a45
Alfred H WU <alfredhwu@gmail.com>
2012-08-16 04:24:29 +0200

    fos userbundle integrated: profile page added

--- src/FTFS/UserBundle/Entity/User.php
+++ src/FTFS/UserBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace FTFS\UserBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -19,6 +20,66 @@ class User extends BaseUser
     protected $id;
 
     /**
+     * @ORM\Column(type="string", length="255")
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="string", length="255")
+     *
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\MinLength(limit="1", message="The name is too short.", groups={"Registration", "Profile"})
+     * @Assert\MaxLength(limit="255", message="The name is too long.", groups={"Registration", "Profile"})
+     */
+    protected $surname;
+
+    /**
+     * @ORM\Column(type="string", length="255")
+     *
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\MinLength(limit="1", message="The name is too short.", groups={"Registration", "Profile"})
+     * @Assert\MaxLength(limit="255", message="The name is too long.", groups={"Registration", "Profile"})
+     */
+    protected $first_name;
+
+    /**
+     * @ORM\Column(type="string", length="255")
+     */
+    protected $company;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $address;
+
+    /**
+     * @Assert\NotBlank(message="Please enter your office phone number.", groups={"Registration", "Profile"})
+     * @ORM\Column(type="string", length="255")
+     */
+    protected $office_phone;
+    
+    /**
+     * @Assert\NotBlank(message="Please enter your office fax number.", groups={"Registration", "Profile"})
+     * @ORM\Column(type="string", length="255")
+     */
+    protected $office_fax;
+
+    /**
+     * @ORM\Column(type="string", length="255")
+     */
+    protected $mobile_phone;
+
+    /**
+     * @ORM\Column(type="string", length="255")
+     */
+    protected $other_phone_1;
+
+    /**
+     * @ORM\Column(type="string", length="255")
+     */
+    protected $other_phone_2;
+
+    /**
      * @ORM\ManyToMany(targetEntity="Group")
      * @ORM\JoinTable(name="ftfs_user_group_relation",
      *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
@@ -31,5 +92,216 @@ class User extends BaseUser
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * Get surname
+     *
+     * @param string surname
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Set first_name
+     *
+     * @param string first_name
+     */
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    /**
+     * Get first_name
+     *
+     * @param string first_name
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+    /**
+     * Set title
+     *
+     * @param string title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @param string title
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set office_phone
+     *
+     * @param string $officePhone
+     */
+    public function setOfficePhone($officePhone)
+    {
+        $this->office_phone = $officePhone;
+    }
+
+    /**
+     * Get office_phone
+     *
+     * @return string 
+     */
+    public function getOfficePhone()
+    {
+        return $this->office_phone;
+    }
+
+    /**
+     * Set mobile_phone
+     *
+     * @param string $mobilePhone
+     */
+    public function setMobilePhone($mobilePhone)
+    {
+        $this->mobile_phone = $mobilePhone;
+    }
+
+    /**
+     * Get mobile_phone
+     *
+     * @return string 
+     */
+    public function getMobilePhone()
+    {
+        return $this->mobile_phone;
+    }
+
+    /**
+     * Set other_phone_1
+     *
+     * @param string $otherPhone1
+     */
+    public function setOtherPhone1($otherPhone1)
+    {
+        $this->other_phone_1 = $otherPhone1;
+    }
+
+    /**
+     * Get other_phone_1
+     *
+     * @return string 
+     */
+    public function getOtherPhone1()
+    {
+        return $this->other_phone_1;
+    }
+
+    /**
+     * Set other_phone_2
+     *
+     * @param string $otherPhone2
+     */
+    public function setOtherPhone2($otherPhone2)
+    {
+        $this->other_phone_2 = $otherPhone2;
+    }
+
+    /**
+     * Get other_phone_2
+     *
+     * @return string 
+     */
+    public function getOtherPhone2()
+    {
+        return $this->other_phone_2;
+    }
+
+    /**
+     * Set company
+     *
+     * @param string $company
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+    }
+
+    /**
+     * Get company
+     *
+     * @return string 
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * Set office_fax
+     *
+     * @param string $officeFax
+     */
+    public function setOfficeFax($officeFax)
+    {
+        $this->office_fax = $officeFax;
+    }
+
+    /**
+     * Get office_fax
+     *
+     * @return string 
+     */
+    public function getOfficeFax()
+    {
+        return $this->office_fax;
+    }
+
+
+    /**
+     * Set address
+     *
+     * @param text $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * Get address
+     *
+     * @return text 
+     */
+    public function getAddress()
+    {
+        return $this->address;
     }
 }
---
+++ src/FTFS/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace FTFS\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilder;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder->add('username')
+                ->add('email', 'email')
+                ->add('plainPassword', 'repeated', array('type' => 'password'))
+                ->add('title', 'choice', array(
+                    'choices' => array(
+                        'Ms.' => 'Ms.',
+                        'Mrs.'  => 'Mrs.',
+                        'Mr.'   => 'Mr.',
+                    ),
+                ))
+                ->add('surname')
+                ->add('first_name')
+                ->add('company', 'choice', array(
+                    'choices' => array(
+                        'Fujitsu Telecom France SAS' => 'Fujitsu Telecom France SAS',
+                        'Orange France' => 'Orange France',
+                        'Bouygue' => 'Bouygue',
+                        'SUN MicroSystem' => 'SUN MicroSystem',
+                        'Fujitsu Japon' => 'Fujitsu Japon',
+                    ),
+                ))
+                ->add('address', null, array(
+                    'required' => false,
+                ))
+                ->add('office_phone')
+                ->add('office_fax', null, array(
+                    'required' => false,
+                ))
+                ->add('mobile_phone', null, array(
+                    'required' => false,
+                ))
+                ->add('other_phone_1', null, array(
+                    'required' => false,
+                ))
+                ->add('other_phone_2', null, array(
+                    'required' => false,
+                ))
+        ;
+    }
+
+    public function getName()
+    {
+        return 'ftfs_user_registration';
+    }
+}
