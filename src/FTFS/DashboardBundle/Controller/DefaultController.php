b4392dfe65d983892c58ca056cddc2c4b9ac5f5b
alfredhwu-ftfs <fujitsukefu@gmail.com>
2012-08-21 16:28:33 +0200

    fos group not working... fork another branch & trying without group

--- src/FTFS/DashboardBundle/Controller/DefaultController.php
+++ src/FTFS/DashboardBundle/Controller/DefaultController.php
@@ -10,14 +10,10 @@ class DefaultController extends Controller
     
     public function indexAction()
     {
-        /*$message = \Swift_Message::newInstance()
-            ->setSubject('Do not replay !!!')
-            ->setFrom('noreply@fujitsu-telecom.fr')
-            ->setTo('alfredhwu@gmail.com')
-            ->setBody('this is an automatic mail send by ftfs support service !!!')
-        ;
-        $this->get('mailer')->send($message);
-         */
-        return $this->render('FTFSDashboardBundle:Default:index.html.twig');
+        if($this->get('security.context')->isGranted('ROLE_ADMIN'))
+        {
+            return $this->render('FTFSDashboardBundle:Default:index.html.twig');
+        }
+        return $this->render('FTFSDashboardBundle:Default:dashboard.html.twig');
     }
 }
